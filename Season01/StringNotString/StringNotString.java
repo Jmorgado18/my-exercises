ed77cc105c3fc15490f8f295343516a2f2b7607e
Joao <jpmorgado18@gmail.com>
2024-09-25 23:03:10 +0100

    Resolvidos conflitos após o pull

---
+++ Season01/StringNotString/StringNotString.java
@@ -0,0 +1,16 @@
+class StringNotString {
+    public static void main(String[] args){
+    String word = "CodeForAll";
+
+    if (word.substring(0,3).equals("not")){
+
+        System.out.println(word);
+
+        }else{
+
+        System.out.println ("not"+word);
+
+        }
+
+    }
+}

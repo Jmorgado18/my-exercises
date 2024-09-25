407744ba199019bb02bd72b79cf342264d6e235b
Joao <jpmorgado18@gmail.com>
2024-09-25 22:49:27 +0100

    Programa Soccer - introduçao a programaçao orientada a objetos

--- Season01/02/ClosestNeighbours/src/ClosestNeighbours.java
+++ Season01/02/ClosestNeighbours/src/ClosestNeighbours.java
@@ -3,19 +3,14 @@ public class ClosestNeighbours {
         int[] numbers = {0, 5, 1209, 6, 2, 111, 112, 33};
         int dif = Math.abs(numbers[0]-numbers[1]);
         int calc;
-        int nr=0;
-        int nr1=0;
         //System.out.println(dif);
 
     for (int i=0; i<numbers.length - 1; i++){
         calc = Math.abs(numbers[i]-numbers[i+1]);
         if (calc < dif){
             dif = calc;
-            nr = numbers[i];
-            nr1 = numbers[i+1];
         }
     }
-    System.out.println("A menor diferença entre numeros vizinhos presentes no array é " + dif + ", entre os numeros " +
-     nr + " e " + nr1);
+    System.out.println("A menor diferença entre numeros vizinhos presentes no array é " + dif);
 }
 }

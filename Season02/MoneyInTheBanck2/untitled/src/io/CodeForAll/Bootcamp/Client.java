f3c7983459abfeafb9da970f872663c0a712b85e
Jmorgado18 <jpmorgado18@gmail.com>
2024-09-30 17:04:13 +0100

    Update Client.java

--- Season02/MoneyInTheBanck2/untitled/src/io/CodeForAll/Bootcamp/Client.java
+++ Season02/MoneyInTheBanck2/untitled/src/io/CodeForAll/Bootcamp/Client.java
@@ -1,15 +1,26 @@
 package io.CodeForAll.Bootcamp;
 
 public class Client {
-    private String nome;
-
-    // Construtor
-    public Client(String nome) {
-        this.nome = nome;
-    }
-
-    // Getter
-    public String getNome() {
-        return nome;
-    }
+private String nome; private Bank bank;
+// Construtor
+public Client (String nome) {
+this.nome = nome;
+this. bank = new
+Bank(this); // Associa o banco ao cliente
 }
+// Métodos de depósito e levantamento, que interagem com o
+Bank
+public void depositar (double
+quantia) {
+bank. Add (quantia);
+}
+public void levantar(double
+quantia) {
+bank. Sub (quantia);
+}
+public double consultarSaldo ( {
+return bank getSaldo ();
+}
+// Getter para o nome
+public String getNome () {
+return nome;

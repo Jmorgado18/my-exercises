9237af69a3fb8b903ba9f548813eb680e4704aa7
João Morgado <jpmorgado18@gmail.com>
2024-10-08 17:51:26 +0100

    Libraries4

--- Season03/01/MyFirstLibrarie/src/Main.java
+++ Season03/01/MyFirstLibrarie/src/Main.java
@@ -1,5 +1,13 @@
+import org.academiadecodigo.simplegraphics.graphics.Color;
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Rectangle rectangle=new Rectangle(2,3,400,400);
+        rectangle.draw();
+        rectangle.setColor(Color.LIGHT_GRAY);
+        rectangle.fill();
+        rectangle.grow(100,100);
+
     }
 }
---
+++ Season03/01/MyFirstLibrarie/src/io/bootcamp/FanStatics/image.java
@@ -0,0 +1,10 @@
+package io.bootcamp.FanStatics;
+
+import org.academiadecodigo.simplegraphics.graphics.Rectangle;
+import org.academiadecodigo.simplegraphics.pictures.Picture;
+
+public class image extends Rectangle {
+
+    Picture picture=new Picture(199,199,"fcp.png");
+
+}

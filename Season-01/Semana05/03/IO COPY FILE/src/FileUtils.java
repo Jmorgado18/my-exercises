94b4c5733fef0014b4b8fa3744d337b1b46ac7cb
João Morgado <jpmorgado18@gmail.com>
2024-10-16 14:11:58 +0100

    File InputStream/OutputStream

---
+++ Season-01/Semana05/03/IO COPY FILE/src/FileUtils.java
@@ -0,0 +1,23 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class FileUtils {
+
+    public static void copyFile(String input,String output) throws IOException {
+        try(
+        FileInputStream inputStream = new FileInputStream(input);
+        FileOutputStream outputStream = new FileOutputStream(output))
+        {
+            byte[]buffer = new byte[1024];
+            int byteReader;
+            while ((byteReader = inputStream.read(buffer)) !=-1){
+                 outputStream.write(buffer,0,byteReader);
+            }
+
+        }
+
+    }
+    }
+
---
+++ Season-01/Semana05/03/IO COPY FILE/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+    try{
+            FileUtils.copyFile("files/Original.docx","files/Copy.DOCX");
+        System.out.println("Tarefa realizada com sucesso");
+
+    } catch (IOException e) {
+        System.out.println("ERRO!");
+       // throw new RuntimeException(e);
+    }
+    }
+}

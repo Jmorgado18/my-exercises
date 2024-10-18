4b24d53bb4cbec2f5abb01be9dde8f54cf07fabd
João Morgado <jpmorgado18@gmail.com>
2024-10-18 09:44:44 +0100

    Paint v2

---
+++ Season-01/Semana05/04/Paintv2/SimpleGraficsPaint/src/Playground.java
@@ -0,0 +1,8 @@
+import io.codeforall.fanstatics.Field;
+
+public class Playground {
+    public static void main(String[] args) {
+        Field field = new Field(20,20);
+        field.DrawRectangle();
+    }
+}

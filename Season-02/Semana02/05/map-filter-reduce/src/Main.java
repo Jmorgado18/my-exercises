66b9cc76ecc0454b24cfdbb2aed66bde6f1b54c1
João Morgado <jpmorgado18@gmail.com>
2024-11-04 12:54:07 +0000

    MAPFILTERREDUCE

---
+++ Season-02/Semana02/05/map-filter-reduce/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        String message = "I'll send an SOS to the garbage world, " +
+                "I hope that someone garbage gets my message in a garbage bottle.";
+    
+
+    }
+}

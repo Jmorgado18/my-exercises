e3a46a4db4ec29a9fab203cdee1e0f311f60941e
Joao <jpmorgado18@gmail.com>
2024-11-26 11:16:31 +0000

    html and Css webPage with navBar, header, menu, body and footer

---
+++ Season-04/Semana02/02/script.js
@@ -0,0 +1,11 @@
+function saudacao() {
+    window.alert('Não confundas lavar o cu no Ribeiro com levar no cu do Ribeiro');
+    window.alert('Não confundas o tratado de Tordesilhas com o tarado atrás das ilhas');
+    window.alert('Não confundas o Santo António feito de pau com o António Santos de pau feito');
+    window.alert('Não confundas panacoota com o meu pau na tua cota');
+    window.alert('Não confundas uma salada de mamão com uma mamada no salão');
+    window.alert('Não confundas a obra do mestre Picasso, com a pica de aço do mestre de obra');
+    window.alert('Não confundas o piloto da marinha com a pilinha do maroto')
+    
+
+}

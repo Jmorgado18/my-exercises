3b9ae50b9e995d0bcc5a7dc141c320bcaceea3ad
Joao <jpmorgado18@gmail.com>
2024-11-27 14:47:27 +0000

    Web Customer Details MVC- JSP file

---
+++ Season-04/Semana02/03/Web MVC Customer Details_exercise/webApp/src/main/java/io/codeforall/fanstatics/Customer.java
@@ -0,0 +1,21 @@
+package io.codeforall.fanstatics;
+
+import com.sun.net.httpserver.HttpServer;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Customer extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/cr7.jsp");
+
+        dispatcher.forward(req, resp);
+    }
+
+}

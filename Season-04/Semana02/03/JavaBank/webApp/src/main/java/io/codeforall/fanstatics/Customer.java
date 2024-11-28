4b337898b8d4c53d3c99fb568db4f0a562b4c8b1
João Morgado <jpmorgado18@gmail.com>
2024-11-28 11:06:03 +0000

    webApp with JSP and CSS JavaBank

--- Season-04/Semana02/03/JavaBank/webApp/src/main/java/io/codeforall/fanstatics/Customer.java
+++ Season-04/Semana02/03/JavaBank/webApp/src/main/java/io/codeforall/fanstatics/Customer.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Customer extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/index.jsp");
@@ -21,6 +22,8 @@ public class Customer extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
       RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/menu.jsp");
       dispatcher.forward(req,resp);
 

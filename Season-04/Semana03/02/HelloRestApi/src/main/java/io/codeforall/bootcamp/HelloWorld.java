befbaf9265cace231a239cb55e10b8cf7b33de8d
João Morgado <jpmorgado18@gmail.com>
2024-12-03 14:09:02 +0000

    HelloWorldRestApi

--- Season-04/Semana03/02/HelloRestApi/src/main/java/io/codeforall/bootcamp/HelloWorld.java
+++ Season-04/Semana03/02/HelloRestApi/src/main/java/io/codeforall/bootcamp/HelloWorld.java
@@ -1,13 +1,11 @@
 package io.codeforall.bootcamp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorld {
 
-    @GetMapping("/api/hello")
+    @RequestMapping(method = RequestMethod.GET, value = "api/hello")
     public String helloWorld(@RequestParam(value = "name", defaultValue = "World") String name) {
         return "Hello " + name;
     }

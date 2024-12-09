5afb3cbcb5764e3650721a6420d249e6a504cf60
Joao <jpmorgado18@gmail.com>
2024-12-09 13:47:08 +0000

    JavaScript change color randomly

--- Season-05/Semana02/01/changecolor.js
+++ Season-05/Semana02/01/changecolor.js
@@ -1,6 +1,6 @@
 
 
-function gerarCorAleatoria() {
+function RandomColor() {
     const letras = "0123456789ABCDEF";
     let cor = "#";
     for (let i = 0; i < 6; i++) {
@@ -17,10 +17,19 @@ function criarColunas() {
     body.style.height = "100vh"; 
 
     for (let i = 0; i < 5; i++) {
-        const cor = gerarCorAleatoria();
+        const cor = RandomColor();
         const coluna = document.createElement("div");
         coluna.style.flex = "1"; 
         coluna.style.backgroundColor = cor;
+        coluna.innerText = cor;
+        coluna.style.textAlign = "center";
+        coluna.style.fontSize = "24px";
+        coluna.style.padding = "10px";
+        coluna.style.color = "white";
+        coluna.style.fontWeight = "bold";
+        coluna.style.textShadow = "2px 2px 4px rgba(0, 0, 0, 0.5)";
+        coluna.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.1)";
+        coluna.style.justifyContent="flex-end";
         body.appendChild(coluna);
     }
 }
